2bb71b47935ad86523c29761f7eb8e354163d105
Вадим Бежков <vadimbezhkov3112@gmail.com>
2021-02-08 22:55:30 +0300

    create class abstract Person

--- ConsoleApp1/Person.cs
+++ ConsoleApp1/Person.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    enum Gender:byte
+    {
+        man,woman
+    }
    abstract class Person
     {
         public string LastName { get; set; }
         public string FirstName { get; set; }
-
+        public int Age { get; set; }
+        public Person(string lastName, string firstName, int Age)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            this.Age = Age;
+        }
     }
 }
